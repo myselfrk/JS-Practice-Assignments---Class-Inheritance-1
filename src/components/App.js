1b5886b18dcf2803cff8093bb8d3ad245f01abde
vaishaliravasoambi <65502403+vaishaliravasoambi@users.noreply.github.com>
2020-11-04 13:25:46 +0530

    c

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+import Circle from "./circle";
+import Shape from "./shape";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Circle />
+        <Shape />
+      </>
+    );
+  }
+}
+
+export default App;
