260959924f138f1547e1a1de99a24200a8cb52e2
vaishaliravasoambi <65502403+vaishaliravasoambi@users.noreply.github.com>
2020-11-04 13:20:45 +0530

    ch

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,13 @@
 // import using require
+let shape = require("./shape");
+class circle extends shape {
+  calculateArea() {
+    console.log("calculate");
+  }
+}
+
+module.export = circle;
 
 // declare class
-
 
 // export class using module.exports
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,16 @@
 // declare class
-
 // export class using module.exports
+class shape {
+  constructor(color) {
+    this.color = "red";
+  }
+  drawShape = () => {
+    console.log("draw a shape");
+  };
+
+  calculateArea = () => {
+    console.log("calculate the area ");
+  };
+}
+
+module.exports = shape;
